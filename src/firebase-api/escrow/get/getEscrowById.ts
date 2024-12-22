2e56f2550053a953c2a5fbd7a4ceca2d55ea0553
michaelsychenko1 <michael.sychenko@vvmd.team>
2024-12-22 19:08:17 +0200

    format and fix

--- src/firebase-api/escrow/get/getEscrowById.ts
+++ src/firebase-api/escrow/get/getEscrowById.ts
@@ -2,20 +2,25 @@ import { db } from "config/firebase";
 import { Escrow } from "custom-types/Escrow";
 import { COLLECTIONS } from "enums";
 
-import { mapEscrowDoc } from "./helpers";
+export default async function getEscrowById(
+  escrowId: string
+): Promise<Escrow | null> {
+  try {
+    const escrowDoc = await db
+      .collection(COLLECTIONS.escrows)
+      .doc(escrowId)
+      .get();
 
-export default async function getEscrowById(escrowId: string): Promise<Escrow | null> {
-    try {
-        const escrowDoc = await db.collection(COLLECTIONS.escrows).doc(escrowId).get();
-
-        if (!escrowDoc.exists) {
-            console.log(`getEscrowById: ${escrowId}, document not found`);
-            return null;
-        }
-
-        return mapEscrowDoc(escrowDoc);
-    } catch (error) {
-        console.error("Error getting escrow by ID:", error);
-        throw new Error("Could not retrieve escrow");
+    if (!escrowDoc.exists) {
+      console.log(`getEscrowById: ${escrowId}, document not found`);
+      return null;
     }
+
+    const data = escrowDoc.data() as Escrow;
+
+    return data;
+  } catch (error) {
+    console.error("Error getting escrow by ID:", error);
+    throw new Error("Could not retrieve escrow");
+  }
 }
